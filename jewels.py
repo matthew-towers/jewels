b8a6af6d0355c564b5e6ce79eb90d05b6e960ed1
Matthew Towers <matthew.towers@gmail.com>
2018-05-30 14:51:01 +0100

    add new strategies, clean up code

--- jewels.py
+++ jewels.py
@@ -2,7 +2,6 @@
 # various strategies
 
 import random
-import copy
 
 #global game parameters. The rockbox jewels game has width=height=8, numberOfColours=7, vanishLength=3
 width = 8
@@ -19,12 +18,12 @@ class board:
 
     def printEntries(self): #only for <10 colours
         for j in range(height):
-            print " ".join(map(str,self.entries[j]))
+            print " ".join(map(str, self.entries[j]))
 
     def randomize(self):
         for i in range(width):
             for j in range(height):
-                self.entries[j][i] = random.randint(1,numberOfColours)
+                self.entries[j][i] = random.randint(1, numberOfColours)
 
     def findMonos(self):
         # return a list whose elements correspond to the maximal monos
@@ -39,45 +38,40 @@ class board:
             row = self.entries[i]
             currentColour = row[0]
             currentStreakStart = 0
-            for j in range(1,width):
+            for j in range(1, width):
                 if row[j] != currentColour:
                     if j - currentStreakStart >= vanishLength:
-                        output.append([ [i,x] for x in
-                            range(currentStreakStart, j) ])
+                        output.append([[i, x] for x in range(currentStreakStart, j)])
                     currentColour = row[j]
                     currentStreakStart = j
             # we hit the last entry, j=width. Was it the last entry of a
             # mono? if so, append it.
             if width - currentStreakStart >= vanishLength:
-                output.append([[i,x] for x in
-                    range(currentStreakStart,width)])
+                output.append([[i, x] for x in range(currentStreakStart, width)])
         #now the vertical monos
         for j in range(width):
             currentColour = self.entries[0][j]
             currentStreakStart = 0
-            for i in range(1,height):
+            for i in range(1, height):
                 if self.entries[i][j] != currentColour:
                     if i - currentStreakStart >= vanishLength:
-                        output.append([ [x,j] for x in
-                            range(currentStreakStart,i)])
+                        output.append([[x, j] for x in range(currentStreakStart, i)])
                     currentColour = self.entries[i][j]
                     currentStreakStart = i
             #we hit the last entry of the column, was it the last entry
             # of a mono? if so, append it to output
             if height - currentStreakStart >= vanishLength:
-                output.append([[x,j] for x in
-                    range(currentStreakStart,height)])
+                output.append([[x, j] for x in range(currentStreakStart, height)])
 
         return output
 
     def gravity(self):
         for j in range(width):
-            col = [self.entries[i][j] for i in range(height) if
-                    self.entries[i][j] != 0]
+            col = [self.entries[i][j] for i in range(height) if self.entries[i][j] != 0]
             newcol = [0 for x in range(height - len(col))] + col
             #make the first col equal to newcol
             for i in range(height):
-                self.entries[i][j]= newcol[i]
+                self.entries[i][j] = newcol[i]
 
     def randomFillZeroes(self):
         for i in range(height):
@@ -92,7 +86,7 @@ class board:
         # gravity
         # random fill any spaces
         #until there are no more monos
-        while(len(self.findMonos()) != 0):
+        while len(self.findMonos()) != 0:
             monos = self.findMonos()
             for m in monos:
                 self.score += len(m) - vanishLength + 1
@@ -112,41 +106,41 @@ class board:
             #self.printEntries()
             #print "\n"
             #end debugging
-    
+
     def horizontalMonoContaining(self, coords):
         #does self.entries contain a horizontal mono containing the entry coords?
         colour = self.entries[coords[0]][coords[1]]
         l = 0
-        while(True):
+        while True:
             if (coords[1]+l >= width) or (self.entries[coords[0]][coords[1]+l] != colour):
                 break
             else:
                 l += 1
         r = 0
-        while(True):
+        while True:
             if (coords[1] - r < 0) or (self.entries[coords[0]][coords[1]-r] != colour):
                 break
             else:
                 r += 1
-        return (l + r - 1 >= vanishLength)
-        
+        return l + r - 1 >= vanishLength
+
     def verticalMonoContaining(self, coords):
         #does self.entries contain a vertical mono containing the entry coords?
         colour = self.entries[coords[0]][coords[1]]
         d = 0
-        while(True):
+        while True:
             if (coords[0] + d >= height) or (self.entries[coords[0] + d][coords[1]] != colour):
                 break
             else:
                 d += 1
         u = 0
-        while(True):
+        while True:
             if (coords[0] - u < 0) or (self.entries[coords[0] - u][coords[1]] != colour):
                 break
             else:
                 u += 1
-        return (u + d - 1 >= vanishLength)
-        
+        return u + d - 1 >= vanishLength
+
     def legitMoves(self):
         #moves swap two entries which are adjacent horizontally or vertically. A move is legit iff it creates a new mono
         #this function returns a list of all legit moves, as a list of pairs of coordinates
@@ -156,23 +150,23 @@ class board:
                 if (j < width - 1) and (self.entries[i][j] != self.entries[i][j+1]):
                 #it makes sense to swap [i,j] and [i,j+1]. does that create a new mono?
                 #copy.deepcopy is really slow, so we'll apply the move, test for monos, then apply it again to get back where we started
-                    self.applyMove([[i,j],[i,j+1]])
+                    self.applyMove([[i, j], [i, j+1]])
                     #now look for monos. There can be four kinds:
                     # 1. vertical mono containing [i,j+1] of colour entries[i][j]
                     # 2. vertical mono containing [i,j] of colour entries[i][j+1]
                     # 3. horizontal mono finishing at [i,j] of colour entries[i][j+1]
                     # 4. horizontal mono starting at [i,j+1] of colour entries[i][j]
-                    if self.verticalMonoContaining([i,j+1]) or self.verticalMonoContaining([i,j]) or self.horizontalMonoContaining([i,j]) or self.horizontalMonoContaining([i,j+1]):
-                        output.append([[i,j], [i,j+1]])
-                    self.applyMove([[i,j],[i,j+1]]) # get self.entries back to where it was before
+                    if self.verticalMonoContaining([i, j+1]) or self.verticalMonoContaining([i, j]) or self.horizontalMonoContaining([i, j]) or self.horizontalMonoContaining([i, j+1]):
+                        output.append([[i, j], [i, j+1]])
+                    self.applyMove([[i, j], [i, j+1]]) # get self.entries back to where it was before
                 if (i < height - 1) and (self.entries[i][j] != self.entries[i+1][j]):
                     #it makes sense to swap [i,j] and [i+1,j]. does that create a new mono?
-                    self.applyMove([[i,j],[i+1,j]])
-                    if self.verticalMonoContaining([i,j]) or self.verticalMonoContaining([i+1,j]) or self.horizontalMonoContaining([i,j]) or self.horizontalMonoContaining([i+1,j]):
-                        output.append([[i,j],[i+1,j]])
-                    self.applyMove([[i,j],[i+1,j]])
+                    self.applyMove([[i, j],[i+1, j]])
+                    if self.verticalMonoContaining([i, j]) or self.verticalMonoContaining([i+1, j]) or self.horizontalMonoContaining([i, j]) or self.horizontalMonoContaining([i+1, j]):
+                        output.append([[i, j], [i+1, j]])
+                    self.applyMove([[i, j], [i+1, j]])
         return output
-    
+
     def applyMove(self, move):
         firstrow = move[0][0]
         firstcol = move[0][1]
@@ -181,7 +175,7 @@ class board:
         temp = self.entries[firstrow][firstcol]
         self.entries[firstrow][firstcol] = self.entries[secondrow][secondcol]
         self.entries[secondrow][secondcol] = temp
-        
+
 
 #strategies:
         # choose randomly from all available moves
@@ -195,22 +189,47 @@ class board:
         # if no move is available, stop and log the score. else
         # choose and make a move
 
-scores = []
-for i in range(100):
-    b = board()
-    b.randomize()
-    while(True):
-        b.evolve()
-        moves = b.legitMoves()
-        if len(moves) == 0:
-            scores.append(b.score)
-            break
-        b.applyMove(random.choice(moves))
 
-mu = sum(scores)/(1.0 * len(scores))
-s2 = sum([(x-mu) ** 2 for x in scores])/ (len(scores)-1)
-print mu, s2 ** 0.5
-# Poisson(100)?
+def testStrategy(chooser, numberOfGames):
+    #a strategy is a way of choosing moves. testStrategy takes 
+    #a function chooser which accepts a list of moves and returns
+    #one of them, and a number numberOfGames, and runs numberOfGames
+    #games using chooser to pick from the available moves
+    #it returns the list of scores from those games
+    scores = []
+    for i in range(numberOfGames):
+        b = board()
+        b.randomize()
+        while True:
+            b.evolve()
+            moves = b.legitMoves()
+            if len(moves) == 0:
+                scores.append(b.score)
+                break
+            b.applyMove(chooser(moves))
+
+    mu = sum(scores)/(1.0 * numberOfGames)
+    s2 = sum([(x-mu) ** 2 for x in scores])/ (numberOfGames - 1)
+    print "n", numberOfGames, "mean", mu, "sd", s2 ** 0.5
+    return scores
+
+
+def randomChooser(moves):
+    return random.choice(moves)
+
+def chooseNearTop(moves):
+    #pick randomly from the 5 moves nearest the top
+    movesSortedByRow = sorted(moves, key=lambda x: x[0][0])
+    return random.choice(movesSortedByRow[:5])
+
+def chooseNearBottom(moves):
+    #pick randomly from the 5 moves nearest the bottom
+    movesSortedByRow = sorted(moves, key=lambda x : x[0][0])
+    return random.choice(movesSortedByRow[-5:])
+
+scores = testStrategy(chooseNearBottom, 1000)
+
+#export scores data in R-readable format
 f = open("/home/matthew/Dropbox/code/python/jewels/scores.txt", "a")
 r_string = "\nx=c(" + ",".join(map(str, scores)) + ")"
 #use source("scores.txt") in R to load this
